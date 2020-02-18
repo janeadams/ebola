47d1e238b6f12c54e6efc0c5c9187a61721ab6b0
jpp46 <jpowers4@uvm.edu>
2020-02-18 12:42:43 -0500

    reformating data to csv files

--- load_data.jl
+++
@@ -1,8 +0,0 @@
-using DataFrames, XLSX
-
-Trigger_NA = DataFrame(XLSX.readtable("data/all_paper_data.xlsx", "Trigger_NA")...)
-Trigger_Ave = DataFrame(XLSX.readtable("data/all_paper_data.xlsx", "Trigger_Ave")...)
-Trigger_Other = DataFrame(XLSX.readtable("data/all_paper_data.xlsx", "Trigger Other")...)
-Follow_Up = DataFrame(XLSX.readtable("data/all_paper_data.xlsx", "Follow Up")...)
-Follow_Up_Other = DataFrame(XLSX.readtable("data/all_paper_data.xlsx", "Follow_Up_Other")...)
-Codebook = DataFrame(XLSX.readtable("data/all_paper_data.xlsx", "Codebook")...)
