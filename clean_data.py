6c87a12efbde7d7cbe0b813961637f3c66d8c095
jack <jfelag@uvm.edu>
2020-03-18 20:29:52 -0400

    script for spellcheck on question data

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,7 @@
 from sklearn.feature_extraction.text import CountVectorizer
-
+import csv
+import enchant
+from gensim.corpora.dictionary import Dictionary
 
 def bag_of_words(data):
     '''
@@ -22,25 +24,78 @@ def bag_of_words(data):
     '''
     model = CountVectorizer(lowercase=True)
     
-    X = model.fit_transform(corpus)
+    X = model.fit_transform(data)
     bag = model.get_feature_names()
     counts = X.toarray()
     
     return bag, counts
-    
-corpus = ["Don't eat my sandwich",
-          "HELLO,, there",
-          "Data Lab is cool!"]
 
-print(bag_of_words(corpus))
 
-with open('data/Follow_Up_Other.csv', 'r') as f:
-    for line in f:
-        pass
+def clean_questions(text):
     
+    d = enchant.Dict("en_US")
+    
+    def f(x):
+        if x.lower() == 'na':
+            return 'NA'
+        
+        if d.check(x):
+            return x
+        
+        try:
+            new = d.suggest(x)[0]
+            return new
+        except:
+            return x
+        
+    str = ''
+    text = text.strip()
+    for word in text.split():
+        str += f(word) + ' '
+        
+    return str
+    
+    
+#corpus = ["Don't eat my sandwich",
+#          "HELLO,, there",
+#          "Data Lab is cool!"]
+
+#print(bag_of_words(corpus))
+
+with open('data/Follow_Up_Other.csv') as csvFile:
+    #0:     Date
+    #1:     District
+    #2:     Chiefdom
+    #3:     Section
+    #4:     community
+    #5-10:  fq#
+    
+    d = enchant.Dict("en_US")
+    
+    reader = csv.reader(csvFile)
+    questions = []
+    for i, line in enumerate(reader):
+        newLine = []
+        if i != 0:
+            for j,q in enumerate(line):
+                if j > 4:
+                    newLine.append(clean_questions(q))
+                else:
+                    newLine.append(q)
+        else:
+            newLine = line
+            
+        questions.append(newLine)
+
+
+with open('data/Follow_Up_Other_CLEANED.csv', 'w') as csvFile:
+    writer = csv.writer(csvFile)
+    for row in questions:
+        writer.writerow(row)
+'''
 with open('data/Trigger_Other.csv', 'r') as f:
     for line in f:
         pass
-    
+''' 
 
 
